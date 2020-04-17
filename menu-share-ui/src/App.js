0fe6606060d45a36156d5ca9f9e20a78a4223575
Sonam Gurung <gurungamar25@gmail.com>
2020-04-17 18:26:11 -0500

    removed cached folder

---
+++ menu-share-ui/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+import MainLayout from "./component/layout/MainLayout";
+
+function App() {
+  return (
+    <div className="App">
+      <MainLayout />
+    </div>
+  );
+}
+
+export default App;
